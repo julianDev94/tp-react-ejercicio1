74edf2fdc9e384fd6e8d503ccf11c58b0aa54e55
JulianAballay <juliancabj1210@gmail.com>
2024-01-29 22:00:55 -0300

    maqueto el componente Mensaje, agregando en app el componente container y dentro el componente Mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Container } from "react-bootstrap";
+import Mensaje from "./components/Mensaje";
 function App() {
-  return <></>;
+  return (
+    <>
+    <Container className="my-3">
+      <Mensaje />
+    </Container>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mensaje = () => {
+    return (
+        <article>
+            <p>Hello world!</p>
+        </article>
+    );
+};
+
+export default Mensaje;
